7c93be5dbb82067b39f88b60e95a82d66f5d446c
Ensky Lin <linhomeyeu@gmail.com>
2015-06-08 22:44:24 +0800

    fix grab regex warning

--- application/helpers/grab_helper.php
+++ application/helpers/grab_helper.php
@@ -7,8 +7,12 @@ function page_convert ($page) {
 
 function unhtmlentities ($string) {
 	// replace numeric entities
-	$string = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $string);
-	$string = preg_replace('~&#([0-9]+);~e', 'chr("\\1")', $string);
+	$string = preg_replace_callback('/&#x([0-9a-f]+);/i', function ($matches) {
+		return chr(hexdec($matches[1]));
+	}, $string);
+	$string = preg_replace_callback('/&#([0-9]+);/', function ($matches) {
+		return chr($matches[1]);
+	}, $string);
 	// replace literal entities
 	$trans_tbl = get_html_translation_table(HTML_ENTITIES);
 	$trans_tbl = array_flip($trans_tbl);
